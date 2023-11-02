90c7c0dd18bc8d3c5aee4a06d26a97921421568c
EdMorales2986 <ed.morales6892@gmail.com>
2023-11-02 16:36:34 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mango-X',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
